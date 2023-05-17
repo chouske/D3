6b23a573540380f52b42f626667bee54f94a3714
Chase Houske <chouske@ucsc.edu>
2023-05-17 12:03:19 -0700

    Cleaned up assets

--- game.js
+++ game.js
@@ -62,7 +62,7 @@ class stage1 extends Phaser.Scene {
         this.load.image('house', 'House.png')
         this.load.image('tree', 'Tree.png')
         this.load.image('apple', 'Apple.png')
-        this.load.image('monkey', 'Monkey.png')
+        this.load.image('human', 'Human.png')
     }
     create(){
         this.platforms = this.physics.add.staticGroup();
@@ -78,7 +78,7 @@ class stage1 extends Phaser.Scene {
         this.apples.create(800, 600, 'apple').setScale(0.5)
         this.apples.create(625, 650, 'apple').setScale(0.5)
         this.apples.create(100, 800, 'apple').setScale(0.5)
-        this.player = this.physics.add.sprite(500, 600, 'monkey').setScale(0.3)
+        this.player = this.physics.add.sprite(500, 600, 'human').setScale(0.3)
         this.player.airjump = false;
         //this.player.setCollideWorldBounds(true);
         //this.physics.add.image(1300, 200, 'apple').setScale(0.5).setBounce(0.2)
@@ -145,30 +145,31 @@ class stage2 extends Phaser.Scene {
     preload(){
         this.load.path = './assets/';
         this.load.image('floor', 'Floor.png')
-        this.load.image('hill1', 'Hillpt1.png')
-        this.load.image('hill2', 'Hillpt2.png')
-        this.load.image('hill3', 'Hillpt3.png')
-        this.load.image('hill4', 'Hillpt4.png')
+        this.load.image('hill', 'Hill.png')
         this.load.image('house', 'House.png')
         this.load.image('tree', 'Tree.png')
         this.load.image('apple', 'Apple.png')
-        this.load.image('monkey', 'Monkey.png')
+        this.load.image('human', 'Human.png')
         this.load.image('cloud', 'Cloud.png')
     }
     create(){
         this.platforms = this.physics.add.staticGroup();
         this.platforms.create(700, 1225, 'floor')
-        this.platforms.create(500, 1050, 'hill1').setSize(402, 56).setScale(2)
+        //this.add.image(500, 760, 'hill').setAlpha(0.9)
+        /*this.platforms.create(500, 1050, 'hill1').setSize(402, 56).setScale(2)
         this.platforms.create(900, 900, 'hill2').setSize(402, 226).setScale(2)
         this.platforms.create(1300, 900, 'hill3').setSize(402, 226).setScale(2)
-        this.platforms.create(1700, 1050, 'hill4').setSize(402, 56).setScale(2)
+        this.platforms.create(1700, 1050, 'hill4').setSize(402, 56).setScale(2)*/
         this.platforms.create(400, 600, 'floor').setSize(240, 35).setScale(0.1)
+        this.platforms.create(900, 800, 'floor').setSize(240, 35).setScale(0.1)
+        this.platforms.create(400, 600, 'floor').setSize(240, 35).setScale(0.1)
+        this.platforms.create(1400, 750, 'floor').setSize(240, 35).setScale(0.1)
         this.add.image(1700, 925, 'tree')
         this.add.image(400, 200, 'cloud')
         this.add.image(1000, 150, 'cloud')
         this.add.image(1600, 250, 'cloud')
         this.apples = this.physics.add.staticGroup();
-        this.apples.create(400, 200, 'apple').setScale(0.5).setCircle(17).setOffset(22, 40)
+        this.apples.create(400, 200, 'apple').setScale(0.5)//.setCircle(17).setOffset(22, 40)
         this.apples.create(1700, 825, 'apple').setScale(0.5)
         this.apples.create(1075, 620, 'apple').setScale(0.5)
         this.apples.create(350, 550, 'apple').setScale(0.5)
@@ -176,7 +177,7 @@ class stage2 extends Phaser.Scene {
         this.platforms.create(1700, 650, 'floor').setSize(240, 35).setScale(0.1)
         this.apples.create(1700, 600, 'apple').setScale(0.5)
         
-        this.player = this.physics.add.sprite(500, 960, 'monkey').setScale(0.3)
+        this.player = this.physics.add.sprite(500, 960, 'human').setScale(0.3)
         this.player.airjump = false;
         this.player.setCollideWorldBounds(true);
         //this.player.setCollideWorldBounds(true);
@@ -188,8 +189,8 @@ class stage2 extends Phaser.Scene {
         this.physics.add.overlap(this.player, this.apples, this.collectapple, null, this)
         this.counter = 0;
         this.curvelocity = 0;
-        this.mytri = this.add.triangle(1000, 500, 100, 100, 200, 100, 150, 50, 0xFFFF00, 1)
-        this.physics.add.existing(this.mytri)
+        //this.mytri = this.add.triangle(1000, 500, 100, 100, 200, 100, 150, 50, 0xFFFF00, 1)
+        //this.physics.add.existing(this.mytri)
     }
     update(){
         const { left, right, up } = this.cursors;
@@ -251,12 +252,13 @@ class stage3 extends Phaser.Scene {
         this.load.image('floor', 'MoonPlatform.png')
         this.load.image('star', 'Star.png')
         this.load.image('apple', 'Apple.png')
-        this.load.image('monkey', 'Monkey.png')
+        this.load.image('human', 'Human.png')
         this.load.image('planet1', 'Planet1.png')
         this.load.image('planet2', 'Planet2.png')
 
     }
     create(){
+        this.cameras.main.setBackgroundColor('#201F3D')
         this.platforms = this.physics.add.staticGroup();
         this.platforms.create(500, 1010, 'floor').setSize(1000, 140).setScale(5)
         this.platforms.create(1500, 1010, 'floor').setSize(1000, 140).setScale(5)
@@ -266,13 +268,15 @@ class stage3 extends Phaser.Scene {
         this.apples.create(1400, 695, 'apple').setScale(0.5)
         this.add.image(1300, 820, 'star').setScale(0.3)
         this.add.image(700, 750, 'star').setScale(0.3)
-        this.platforms.create(400, 300, 'planet1').setSize(104, 99)
-        this.apples.create(425, 205, 'apple').setScale(0.5)
+        this.platforms.create(400, 300, 'planet1').setCircle(95).setOffset(10, 5)
+        this.platforms.create(1600, 250, 'planet2').setCircle(95).setOffset(10, 5)
+        this.apples.create(425, 185, 'apple').setScale(0.5)
+        this.apples.create(135, 115, 'apple').setScale(0.5)
         this.add.image(100, 150, 'star').setScale(0.5)
         this.platforms.create(500, 650, 'floor').setSize(400, 56).setScale(2)
         this.platforms.create(1300, 750, 'floor').setSize(400, 56).setScale(2)
         this.platforms.create(990, 375, 'floor').setSize(400, 56).setScale(2)
-        this.player = this.physics.add.sprite(500, 850, 'monkey').setScale(0.3)
+        this.player = this.physics.add.sprite(500, 850, 'human').setScale(0.3)
         this.player.airjump = false;
         this.player.setCollideWorldBounds(true);
         this.cursors = this.input.keyboard.createCursorKeys();
@@ -321,7 +325,7 @@ class stage3 extends Phaser.Scene {
             this.player.airjump = false;
             this.player.setVelocityY(-430);
         }
-        if(this.counter == 6){
+        if(this.counter == 5){
             this.scene.start('end')
         }
         
@@ -340,7 +344,7 @@ const config = {
     height: 1080,
     backgroundColor: 0x87CEEB,
     //scene: stage1,
-    scene: [stage2, stage1, middle, stage3, middle2, intro, end],
+    scene: [stage3, stage1, middle, stage2, middle2, intro, end],
     physics: {
         default: 'arcade',
         arcade: {
